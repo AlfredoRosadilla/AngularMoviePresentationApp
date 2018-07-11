b024f86b9771958f44fa77224b1e0d07be3e68a1
AlfredoRosadilla <alfredo.rosadilla@globant.com>
2018-07-10 22:05:38 -0300

    - Added manual doc. - Added test to alert and footer component. - Included some fixes.

--- src/app/shared/components/alert/alert.component.spec.ts
+++ src/app/shared/components/alert/alert.component.spec.ts
@@ -38,10 +38,133 @@ describe('AlertComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AlertComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    component.classes = '';
+    component.type = 'error';
+    component.text = 'error text';
+    component.solution = 'error solution';
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Base tests', () => {
+    beforeEach(() => {
+      fixture.detectChanges();
+    });
+
+    it('should create component', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should have a defined component', () => {
+        expect(component).toBeDefined();
+    });
   });
+
+  describe('Properties tests', () => {
+    beforeEach(() => {
+      fixture.detectChanges();
+    });
+
+    it('should have a property icon', () => {
+      expect(component.icon).toBeDefined();
+    });
+  });
+
+  describe('Methods', () => {
+    describe('ngOnInit', () => {
+      it('should exist a method called ngOnInit', () => {
+        fixture.detectChanges();
+
+        expect(component.ngOnInit).toBeDefined();
+      });
+
+      it('should call changeType method', () => {
+        spyOn(component, 'changeType');
+
+        fixture.detectChanges();
+
+        expect(component.changeType).toHaveBeenCalled();
+      });
+    });
+
+    describe('changeType', () => {
+      it('should exist a method called changeType', () => {
+        fixture.detectChanges();
+
+        expect(component.changeType).toBeDefined();
+      });
+
+      it('should change icon type', () => {
+        fixture.detectChanges();
+
+        expect(component.icon).toEqual('warning');
+
+        component.changeType('success');
+        expect(component.icon).toEqual('check');
+
+        component.changeType('information');
+        expect(component.icon).toEqual('info');
+      });
+
+      it('shouldn\'t change icon type', () => {
+        fixture.detectChanges();
+
+        component.changeType('something');
+        expect(component.icon).toEqual('warning');
+      });
+    });
+
+    describe('getTranslateBase', () => {
+      it('should exist a method called getTranslateBase', () => {
+        fixture.detectChanges();
+
+        expect(component.getTranslateBase).toBeDefined();
+      });
+
+      it('should transform strings', () => {
+        const translations = {
+          'Pepe': 'pepe',
+          'alert0Fixed': 'alert0fixed',
+          'SOMETHING SOMETHING ELSE': 'something-something-else'
+        };
+
+        fixture.detectChanges();
+
+        Object.keys(translations).forEach((str: string) => {
+          expect(component.getTranslateBase(str)).toEqual(translations[str]);
+        })
+      });
+    });
+
+    describe('close', () => {
+      it('should exist a method called close', () => {
+        fixture.detectChanges();
+
+        expect(component.close).toBeDefined();
+      });
+
+      it('should call closeAlert Output', () => {
+        spyOn(component.closeAlert, 'emit');
+
+        fixture.detectChanges();
+
+        component.close();
+
+        expect(component.closeAlert.emit).toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe('DOM test', () => {
+    it('should has an element with input classes', (done: any) => {
+      const classes = 'something somethingelse somethingelseelse';
+
+      component.classes = classes;
+
+      fixture.whenStable().then(() => {
+        fixture.detectChanges();
+
+        expect(fixture.nativeElement.querySelector(`.${classes.replace(new RegExp(' ', 'g'), '.')}`)).toBeDefined();
+        done();
+      })
+    })
+  })
 });
--- src/app/shared/components/footer/footer.component.spec.ts
+++ src/app/shared/components/footer/footer.component.spec.ts
@@ -1,19 +1,28 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 
-import { ActivatedRoute } from '@angular/router';
+import { of, throwError } from 'rxjs';
+import { Injector } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 
-import { ServicesModule } from '@app/services';
 import { SharedModule, FooterComponent } from '@app/shared';
+import { ServicesModule, MoviesService, AlertService } from '@app/services';
 
 describe('FooterComponent', () => {
+  let injector: Injector;
+  let mockRouter: any;
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
 
   beforeEach(async(() => {
+    mockRouter = {
+      navigate: jasmine.createSpy('navigate'),
+      events: of(new NavigationEnd(0, '/detail', '/something')),
+      url: '/something'
+    };
     TestBed.configureTestingModule({
       imports: [
         HttpClientModule,
@@ -29,18 +38,100 @@ describe('FooterComponent', () => {
       providers: [{
         provide: ActivatedRoute,
         useClass: class { firstChild = jasmine.createSpy('firstChild'); }
+      }, {
+        provide: Router,
+        useValue: mockRouter
       }]
     })
     .compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(inject([Injector], (_injector: Injector) => {
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    injector = _injector;
+  }));
+
+  describe('Base tests', () => {
+    beforeEach(() => {
+      fixture.detectChanges();
+    });
+
+    it('should create component', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should have a defined component', () => {
+        expect(component).toBeDefined();
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Methods', () => {
+    describe('ngOnInit', () => {
+      it('should exist a method called ngOnInit', () => {
+        fixture.detectChanges();
+
+        expect(component.ngOnInit).toBeDefined();
+      });
+    });
+
+    describe('isCurrentView', () => {
+      it('should exist a method called isCurrentView', () => {
+        fixture.detectChanges();
+
+        expect(component.isCurrentView).toBeDefined();
+      });
+
+      it('should return true', () => {
+        fixture.detectChanges();
+
+        expect(component.isCurrentView('detail')).toBeTruthy();
+      });
+
+      it('should return false', () => {
+        fixture.detectChanges();
+
+        expect(component.isCurrentView('other')).toBeFalsy();
+      });
+    });
+
+    describe('executeAction', () => {
+      it('should exist a method called executeAction', () => {
+        fixture.detectChanges();
+
+        expect(component.executeAction).toBeDefined();
+      });
+
+      it('should call to MoviesService setFavorite method', () => {
+        const moviesService = injector.get(MoviesService);
+
+        spyOn(moviesService, 'setFavorite').and.returnValue(of(true));
+
+        mockRouter.url = '/detail';
+
+        fixture.detectChanges();
+
+        component.executeAction('right-button');
+
+        expect(moviesService.setFavorite).toHaveBeenCalled();
+      });
+
+      it('should call to AlertService showAlert method', (done: any) => {
+        const moviesService = injector.get(MoviesService);
+        const alertService = injector.get(AlertService);
+
+        spyOn(moviesService, 'setFavorite').and.returnValue(throwError('Generic error'));
+        spyOn(alertService, 'showAlert').and.returnValue(true);
+
+        fixture.detectChanges();
+
+        component.executeAction('right-button');
+
+        moviesService.setFavorite(0, true).subscribe(() => {}, () => {
+          expect(alertService.showAlert).toHaveBeenCalled();
+          done();
+        })
+      });
+    });
   });
 });
